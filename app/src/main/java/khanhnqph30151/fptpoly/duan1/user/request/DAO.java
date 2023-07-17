36208c0d70d4e0665bf022249ac239f9c2537f8d
LanLemon17 <lanbn03@gmail.com>
2023-07-17 11:30:03 +0700

    code_cualan

---
+++ app/src/main/java/khanhnqph30151/fptpoly/duan1/user/request/DAO.java
@@ -0,0 +1,68 @@
+package khanhnqph30151.fptpoly.duan1.user.request;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+import khanhnqph30151.fptpoly.duan1.data.DbHelper;
+
+public class DAO {
+    private DbHelper dbHelper;
+SQLiteDatabase sqLiteDatabase;
+    public DAO(Context context) {
+        dbHelper = new DbHelper(context);
+        sqLiteDatabase =dbHelper.getWritableDatabase();
+    }
+    @SuppressLint("Range")
+    public ArrayList<Request> getDataRequest(){
+        ArrayList<Request> list = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tbl_request",null);
+        while (cursor.moveToNext()){
+            Request request = new Request();
+            request.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex("request_id"))));
+            request.setTen((cursor.getString(cursor.getColumnIndex("request_name"))));
+            request.setEmail((cursor.getString(cursor.getColumnIndex("request_email"))));
+            request.setSodienthoai((cursor.getString(cursor.getColumnIndex("request_phone"))));
+            request.setNoidung((cursor.getString(cursor.getColumnIndex("request_content"))));
+            list.add(request);
+        }
+        return list;
+    }
+
+//    public ArrayList<Request> GetRequest() {
+//        ArrayList<Request> list = new ArrayList<>();
+//
+//        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tbl_request", null);
+//        while (cursor.moveToNext()){
+//            Request ph = new Request();
+//            ph.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex("request_id"))));
+//        }
+//        if (cursor.getCount() > 0) {
+//            cursor.moveToFirst();
+//            do {
+//                Request ph = new Request();
+//                //add_rq_name, add_rq_phone,add_rq_email, add_rq_content
+//                ph.setId(Integer.parseInt(cursor.getString(0)));
+//                ph.setSodienthoai(cursor.getString(1));
+//                ph.setEmail(cursor.getString(2));
+//                ph.setNoidung(cursor.getString(3));
+//                list.add(ph);
+//            } while (cursor.moveToNext());
+//        }
+//        return list;
+//    }
+
+    public long AddRQ(Request ph) {
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("request_name", ph.getTen());
+        contentValues.put("request_email", ph.getEmail());
+        contentValues.put("request_phone", ph.getSodienthoai());
+        contentValues.put("request_content", ph.getNoidung());
+        return sqLiteDatabase.insert("tbl_request", null, contentValues);
+    }
+}
--- app/src/main/java/khanhnqph30151/fptpoly/duan1/user/request/RequestFragment.java
+++ app/src/main/java/khanhnqph30151/fptpoly/duan1/user/request/RequestFragment.java
@@ -1,66 +1,94 @@
 package khanhnqph30151.fptpoly.duan1.user.request;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 import khanhnqph30151.fptpoly.duan1.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RequestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RequestFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Context context;
+    private DAO dao =new DAO(context);
+    private ArrayList<Request> arrayListRq = new ArrayList<>();
+    private EditText user_rq_name, user_rq_email, user_rq_phone, user_rq_content;
+    private Button btn_user_rq_send;
+    private View mView;
 
     public RequestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RequestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RequestFragment newInstance(String param1, String param2) {
         RequestFragment fragment = new RequestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request, container, false);
+
+        mView = inflater.inflate(R.layout.fragment_request, container, false);
+
+        user_rq_name = mView.findViewById(R.id.user_rq_name);
+        user_rq_email = mView.findViewById(R.id.user_rq_email);
+        user_rq_phone = mView.findViewById(R.id.user_rq_phone);
+        user_rq_content = mView.findViewById(R.id.user_rq_content);
+        btn_user_rq_send = mView.findViewById(R.id.btn_user_rq_send);
+        btn_user_rq_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendRequestfromUserforAdmin();
+            }
+        });
+
+        return mView;
+         }
+    private void sendRequestfromUserforAdmin() {
+        String add_rq_name = user_rq_name.getText().toString();
+        String add_rq_email = user_rq_email.getText().toString();
+        String add_rq_phone = user_rq_phone.getText().toString();
+        String add_rq_content = user_rq_content.getText().toString();
+        if (add_rq_name.length() == 0) {
+            user_rq_name.requestFocus();
+            user_rq_name.setError("Không được để trống tên");
+        } else if (add_rq_email.length() == 0) {
+            user_rq_email.requestFocus();
+            user_rq_email.setError("Không được để trống email");
+        } else if (add_rq_phone.length() == 0) {
+            user_rq_phone.requestFocus();
+            user_rq_phone.setError("Không được để trống số điện thoại");
+        } else if (add_rq_content.length() == 0) {
+            user_rq_content.requestFocus();
+            user_rq_content.setError("Không được để trống nội dung");
+        } else {
+            Request listResquest = new Request();
+            if (dao.AddRQ(listResquest) > 0) {
+                Toast.makeText(mView.getContext(), "Thêm thành công", Toast.LENGTH_SHORT).show();
+                dao = new DAO(context);
+                arrayListRq = dao.getDataRequest();
+            } else {
+                Toast.makeText(mView.getContext(), "Thêm thất bại", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
